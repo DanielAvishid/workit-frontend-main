2c5ba2f42bfefa2872604c5b2644ef1dd59c24e7
Daniel Avishid <141136256+DanielAvishid@users.noreply.github.com>
2025-04-25 11:20:27 +0300

    Update http.service.js

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/api/'
+    ? 'https://honest-babita-danielavishid-d7d4d2da.koyeb.app/api/'
     : '//localhost:3030/api/'
 
 
